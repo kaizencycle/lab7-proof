e01c2ee8c45840d63ad85fdad4159163ce67f9ee
kaizencycle <michaeljjudan@gmail.com>
2025-10-13 21:18:53 -0400

    Update next.config.js

--- frontend/reflections-app/next.config.js
+++ frontend/reflections-app/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
+};
+module.exports = nexConfig;
   trailingSlash: true,
   images: {
     unoptimized: true
   }
 }
 
-module.exports = nextConfig
+
